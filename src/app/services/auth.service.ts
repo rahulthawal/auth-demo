254ada368756920bf822e4cec732971801084cbf
Rahul A Thawal <rthawal@codecraftworks.com>
2018-04-12 16:59:04 -0400

    User Role
    Hiding elements based on User Role.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -8,14 +10,35 @@ export class AuthService {
 
   login(credentials) { 
    return this.http.post('/api/authenticate', 
-      JSON.stringify(credentials));
+      JSON.stringify(credentials))
+      .map(response => {
+        //console.log(response.json());
+        let result = response.json();
+        if(result && result.token){
+          localStorage.setItem('token', result.token);
+          return true;
+        }
+        return false;
+
+      });
+      //console.log(credentials);
   }
 
   logout() { 
+    localStorage.removeItem('token');
   }
 
   isLoggedIn() { 
-    return false;
+    //let jwtHelper = new JwtHelper();
+    return tokenNotExpired();
+  }
+
+  get currentUser(){
+
+    let token = localStorage.getItem('token');
+    if (!token) return null;
+
+    return new JwtHelper().decodeToken(token);
   }
 }
 
