f444ea21cb862e22e4c2412a4c3718332f5ec6e7
Rahul A Thawal <rthawal2014@my.fit.edu>
2018-04-15 20:06:06 -0400

    Current User
    Token for non admin, Current user

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -6,7 +6,7 @@ export function fakeBackendFactory(
     options: BaseRequestOptions) {
         
   let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6dHJ1ZX0.iy8az1ZDe-_hS8GLDKsQKgPHvWpHl0zkQBqy1QIPOkA';
-    
+  //let token ='IxMjM0NTY3ODkwIiwibmFtZSI6Ik1vc2ggSGFtZWRhbmkiLCJhZG1pbiI6ZmFsc2V9.oEyti9UbZt78ejCXw9Ocv20zoqXp6ZWNMXDI_b6oICk';  
   backend.connections.subscribe((connection: MockConnection) => {
     // We are using the setTimeout() function to simulate an 
     // asynchronous call to the server that takes 1 second. 
